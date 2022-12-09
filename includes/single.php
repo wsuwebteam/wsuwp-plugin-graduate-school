5a1e3cbd418ee3a9d4734a29fdf25ca60c5d6f21
Danial Bleile <danbleile@gmail.com>
2022-12-09 15:07:04 -0800

    Bug Fix | Try removing Filter

--- includes/single.php
+++ includes/single.php
@@ -4,12 +4,14 @@ class Single{
 	
 	public static function init() {
 
-		add_filter( 'the_content', array( __CLASS__, 'display_fact_sheet' ) );
+		add_filter( 'the_content', array( __CLASS__, 'display_fact_sheet' ), 9 );
 
 	}
 
 
 	public static function display_fact_sheet( $content ) {
+
+        remove_filter( 'the_content', array( __CLASS__, 'display_fact_sheet' ), 9 );
 
         if ( is_main_query() && is_singular( 'gs-factsheet' ) ) {
 
